40144a0a832e22c1dfb4f39e4ccd2e4f18495bf5
Tan Pham <minhtantanhiep@gmail.com>
2026-01-20 10:03:32 +0700

    Update database user credentials and remove Ingress configuration

--- initdb/0_create-dbs.sh
+++ initdb/0_create-dbs.sh
@@ -2,7 +2,18 @@
 set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-  CREATE DATABASE n8n;
-  CREATE DATABASE jiradb;
-  CREATE DATABASE confluencedb;
+  -- Create n8n database and user
+  CREATE USER n8n_user WITH PASSWORD 'n8n_password';
+  CREATE DATABASE n8n OWNER n8n_user;
+  GRANT ALL PRIVILEGES ON DATABASE n8n TO n8n_user;
+  
+  -- Create Jira database and user
+  CREATE USER jira_user WITH PASSWORD 'jira_password';
+  CREATE DATABASE jiradb OWNER jira_user;
+  GRANT ALL PRIVILEGES ON DATABASE jiradb TO jira_user;
+  
+  -- Create Confluence database and user
+  CREATE USER confluence_user WITH PASSWORD 'confluence_password';
+  CREATE DATABASE confluencedb OWNER confluence_user;
+  GRANT ALL PRIVILEGES ON DATABASE confluencedb TO confluence_user;
 EOSQL
